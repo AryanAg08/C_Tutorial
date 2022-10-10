9c98df0aac991d484690c5bba6bfca8c952bb5fe
Aryan Goyal <aryangoyal90@outlook.com>
2022-10-10 14:47:19 +0530

    V1.2

---
+++ src/Day1/10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int numA;
+    
+     printf("Enter the three digit number\n");
+     scanf("%d", &numA);
+     
+     int A = numA % 10;
+     int B = numA - A;
+     int C = 
+     
+    
+    return 0;
+}
+
+// incomplete still;
