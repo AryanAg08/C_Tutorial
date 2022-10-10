a98d99f19c2119ac0b7ad2de29b327eba7b8f748
Aryan Goyal <aryangoyal90@outlook.com>
2022-10-10 14:05:08 +0530

    V1.2

---
+++ src/Day1/6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main () {
+    int numA;
+    int numB;
+    printf("Please enter first value \n");
+    scanf("%d", &numA);
+    printf("Please enter second value \n");
+    scanf("%d", &numB);
+    
+    
+    
+    printf("The value of first number is %d \n", numA + numB - numA);
+    printf("The value of second number is %d \n", numA - numB + numB);
+    
+    return 0;
+}
