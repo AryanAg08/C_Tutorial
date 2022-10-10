e8092ce6b070ef01686ce0bbe24cfc4de8ebfb3a
Aryan Goyal <aryangoyal90@outlook.com>
2022-10-10 13:28:56 +0530

    V1.2

---
+++ src/Day1/3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int area;
+     printf("To find the area of the circle!! \n Please enter the radius of circle? \n");
+     scanf("%d", &area);
+     printf("The area of the circle is %f", 3.14 * area * area);
+
+    return 0;
+}
