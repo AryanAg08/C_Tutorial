60b1286c72a14cd324d43fecfda686890001d396
Aryan Goyal <aryangoyal90@outlook.com>
2022-10-10 14:34:45 +0530

    V1.2

--- src/Day1/8.c
+++ src/Day1/8.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int numA, numB, numC, numD;
+    int numA, numB, numC, numD, numE, numF;
     
      printf("Enter the point A \n");
      scanf("%d %d", &numA, &numB);
@@ -11,10 +11,19 @@ int main() {
      scanf("%d %d", &numC, &numD);
   //   printf("%d %d", numC, numD);
      
+     printf("Enter the point C \n");
+     scanf("%d %d", &numE, &numF);
+     
+     
      int PointA = numA - numB;
      int PointB = numC - numD;
+     int PointC = numE - numF;
      
-     printf("Distance between two points is %d", (PointA * PointA) + (PointB * PointB));
+     int distanceAC = (PointA * PointA) + (PointC * PointC);
+     int distanceBC = (PointB * PointB) + (PointC * PointC);
+     
+     printf("Distance between AC points is %d \n\n", distanceAC);
+     printf("Distance between BC points is %d \n", distanceBC);
      
     return 0;
 }
