f1a091ca43154403e00dc4873c448d898b96f2a4
Aryan Goyal <aryangoyal90@outlook.com>
2022-11-17 08:51:16 +0530

    V1.3

--- ClassWork/work2.c
+++ ClassWork/work2.c
@@ -1,4 +1,27 @@
-/*
-Reverse a String.
-Hello ==> olleH
-*/
+#include <stdio.h>
+
+int main()
+{
+   char s[10], r[10];
+   int begin, end, count = 0;
+
+   printf("Input a string\n");
+   gets(s);
+
+   while (s[count] != '\0')
+      count++;
+
+   end = count - 1;
+
+   for (begin = 0; begin < count; begin++) {
+      r[begin] = s[end];
+      end--;
+   }
+
+   r[begin] = '\0';
+
+  printf("Here is the revrse of the string: ");
+   printf("%s\n", r);
+
+   return 0;
+}
