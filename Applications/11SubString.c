4cfcb78b327118761ba86121591bb1bef70c491a
Aryan Goyal <aryangoyal90@outlook.com>
2022-12-09 10:48:06 +0530

    V1.3

--- Applications/11SubString.c
+++ Applications/11SubString.c
@@ -8,20 +8,20 @@ void main () {
 
     printf("Enter string 2: ");
     gets(b1);
-    int i =0; 
+    int i =0;  // 11
   while (a1[i] != '\0')
   i++;
 
-int j = 0;
+int j = 0;   // 3
   while (b1[j] != '\0')
   j++;
 
   int a,b,flag;
 
-  for (a =0; a <= i -j; a++) {
-    for (b = a; b <a + j; b++){
+  for (a =0; a <= i -j; a++) {  // 11 - 3 = 8
+    for (b = a; b <a + j; b++){  // b = 0 ; b < 0 + 3 = 3  
       flag = 1;
-      if (a1[b] != b1[b - a]) {
+      if (a1[b] != b1[b - a]) {  // a1[0] != b1[0 - 0] 
         flag = 0;
         break;
       }
